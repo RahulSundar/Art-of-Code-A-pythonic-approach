3e4a76f4548e20ba25e6c4602b9e6eb1562c4823
Rahul Sundar <rahul.sundar95@gmail.com>
2020-11-09 21:12:39 +0530

    Reverted to the module structure before implementing OOP

--- Projects/Project_1/Binary/base_a_decryption.py
+++ Projects/Project_1/Binary/base_a_decryption.py
@@ -1,81 +1,76 @@
 # Decrypts any code given in a string format and obtains the encoded number. 
 
-class BaseDecrypt:
 
-    def __init__(self,base = 2, system = "LE"):
-        self.system = system
-        self.base = base
+def binary_decryptor(code, system="LE"):
+    """
+    Returns the encoded number provided the binary code string is given.
+    Arguments:
+        : 1. code: A string containing the binary digits
+        : 2. system: A flag to specify either big or little endian systems
+    Return:
+        : number    
+    """
 
-    def binary_decryptor(self, code):
-        """
-        Returns the encoded number provided the binary code string is given.
-        Arguments:
-            : 1. code: A string containing the binary digits
-            : 2. system: A flag to specify either big or little endian systems
-        Return:
-            : number    
-        """
+    number = 0
+    code = str(code)
+    if self.system  == 'BE':
+        for index in range(len(code)):
+            number += int(code[index])*(2**index)
+        return number
+    elif self.system == 'LE':
+        for index in range(len(code)):
+            number += int(code[index])*(2**(len(code)-1-index))
+        return number
+    else:
+        print("ERROR: Choose a valid system")
 
-        number = 0
-        code = str(code)
-        if self.system  == 'BE':
+def binary_decryptorV2(system = "LE"):
+    """
+    Returns the encoded number provided the binary code string is given.
+    Arguments:
+        : 1. code: A string containing the binary digits
+        : 2. system: A flag to specify either big or little endian systems
+    Return:
+        : number    
+    """
+    code =input("Enter the binary code: ")
+    number = 0
+    if system  == 'BE':
+        for index in range(len(code)):
+            number += int(code[index])*(2**index)
+        return number
+    elif system == 'LE':
+        for index in range(len(code)):
+            number += int(code[index])*(2**(len(code)-1-index))
+        return number
+    else:
+        print("ERROR: Choose a valid system")
+
+
+
+def base_a_decryptor(base,code, system="LE"):
+    """
+    Returns the encoded number provided the base and the code string is given.
+    Arguments:
+        : 1. code: A string containing the encoded digits
+        : 2. base: The base in the number has been encoded
+        : 3. system: A flag to specify either big or little endian systems
+    Return:
+        : number    
+    """
+    
+    number = 0
+    if base <= 10:
+        if system  == 'BE':
             for index in range(len(code)):
-                number += int(code[index])*(2**index)
+                number += int(code[index])*(base**index)
             return number
-        elif self.system == 'LE':
+        elif system == 'LE':
             for index in range(len(code)):
-                number += int(code[index])*(2**(len(code)-1-index))
+                number += int(code[index])*(base**(len(code)-1-index))
             return number
-        else:
-            print("ERROR: Choose a valid system")
-
-    def binary_decryptorV2(self):
-        """
-        Returns the encoded number provided the binary code string is given.
-        Arguments:
-            : 1. code: A string containing the binary digits
-            : 2. system: A flag to specify either big or little endian systems
-        Return:
-            : number    
-        """
-        code =input("Enter the binary code: ")
-        number = 0
-        if self.system  == 'BE':
-            for index in range(len(code)):
-                number += int(code[index])*(2**index)
-            return number
-        elif self.system == 'LE':
-            for index in range(len(code)):
-                number += int(code[index])*(2**(len(code)-1-index))
-            return number
-        else:
-            print("ERROR: Choose a valid system")
-
-
-
-    def base_a_decryptor(self, code):
-        """
-        Returns the encoded number provided the base and the code string is given.
-        Arguments:
-            : 1. code: A string containing the encoded digits
-            : 2. base: The base in the number has been encoded
-            : 3. system: A flag to specify either big or little endian systems
-        Return:
-            : number    
-        """
-        
-        number = 0
-        if self.base <= 10:
-            if self.system  == 'BE':
-                for index in range(len(code)):
-                    number += int(code[index])*(self.base**index)
-                return number
-            elif self.system == 'LE':
-                for index in range(len(code)):
-                    number += int(code[index])*(self.base**(len(code)-1-index))
-                return number
-        else:
-            print("Base greater than 10. Please choose a base less than 10")
+    else:
+        print("Base greater than 10. Please choose a base less than 10")
 
 #import gradio as gr 
 
--- Projects/Project_1/Character/Character_encryptor.py
+++ Projects/Project_1/Character/Character_encryptor.py
@@ -1,44 +1,42 @@
-class CharEnc:
+
+def character_counter(string):
+    string = str(string)
+    dict_char_count = dict({i:string.count(i) for i in string})
+    
+    return dict_char_count
 
 
-    def character_counter(self, string):
-        string = str(string)
-        dict_char_count = dict({i:string.count(i) for i in string})
+def character_counter_v2(string):
+    string = str(string)
+    dict_char_count = dict({i:string.count(i) for i in string if string.count(i) > 1})
+    
+    return dict_char_count
+
+def character_count_encryptor_v2(string):
+    dictionary = character_counter(string)
+    code = ""
+    for keys,values in dictionary.items():
+        code += keys + str(values)
         
-        return dict_char_count
+    return code
 
-
-    def character_counter_v2(self, string):
-        string = str(string)
-        dict_char_count = dict({i:string.count(i) for i in string if string.count(i) > 1})
+def character_count_encryptor_v4():
+    string = input("Enter data to be codified: ")
+    dictionary = character_counter(string)
+    code = ""
+    for keys,values in dictionary.items():
+        code += keys + str(values)
         
-        return dict_char_count
-
-    def character_count_encryptor_v2(self, string):
-        dictionary = self.character_counter(string)
-        code = ""
-        for keys,values in dictionary.items():
-            code += keys + str(values)
-            
-        return code
-
-    def character_count_encryptor_v4(self):
-        string = input("Enter data to be codified: ")
-        dictionary = self.character_counter(string)
-        code = ""
-        for keys,values in dictionary.items():
-            code += keys + str(values)
-            
-        return code
+    return code
 
 
-    def character_count_encryptor_v3(self,sentence):
+def character_count_encryptor_v3(sentence):
 
-        word_list = sentence.split()
-        sentence_code = ""
-        for index in range(len(word_list)):
-            sentence_code += self.character_count_encryptor_v2(word_list[index]) + " "
-        return sentence_code
+    word_list = sentence.split()
+    sentence_code = ""
+    for index in range(len(word_list)):
+        sentence_code += character_count_encryptor_v2(word_list[index]) + " "
+    return sentence_code
 
 
 
