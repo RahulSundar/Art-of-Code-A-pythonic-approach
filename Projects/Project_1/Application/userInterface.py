74da7f11cd6bd1c7ec906ec9bb45f874289dbfcc
Rahul Sundar <rahul.sundar95@gmail.com>
2020-11-10 21:43:09 +0530

    Added a skeletal framework for userinterface

--- Projects/Project_1/Application/userInterface.py
+++ Projects/Project_1/Application/userInterface.py
@@ -16,3 +16,61 @@ from BackendEngines.encryptionEngine import EncryptionEngine
 from BackendEngines.validationGenerationEngine import ValidationGenerationEngine
 
 from Utils.filehandler import FileHandler
+
+
+
+#Login interface:
+#---------------#
+
+#Features:
+#--------#
+#1. Ask for UID, Pwd
+
+if __name__ == "__main__":
+
+
+
+    waiting_for_input = True
+    
+
+    while(waiting_for_input):
+        print("-----------------------------------")    
+        print("    Following are the options\n")
+        print("-----------------------------------")
+        print("1 - Login (Maximum three attempts)\n")
+        print("2 - Reset Password\n")
+        print("3 - Register as new user\n")
+        print("Q - Quit\n")
+        print("-----------------------------------")
+        user_input = input("Enter your choice here: \n")
+        
+        if user_input == "1":
+            ctr = 0
+            status = "Login"
+            user_id = input("User ID:- ")
+            password = input("Password:- ")
+            validator = ValidationGenerationEngine(status = "Login", userid = user_id, password = password)
+            while(ctr<3):
+                if validator.validateCredentials():
+                    print("-----------------------------------")
+                    print("      Logged in Successfully")
+                    print("-----------------------------------")
+                else:
+                    ctr += 1
+                    print("---------------------------------------------------------------------------------------------------")
+                    print("Please try again. Maximum three attempts are allowed. This is your " + str(ctr+1) + "th attempt.\n")
+                    print("---------------------------------------------------------------------------------------------------")
+                    status = "Login"
+                    user_id = input("User ID:- ")
+                    password = input("Password:- ")
+                    validator = ValidationGenerationEngine(status = "Login", userid = user_id, password = password)
+                    validator.validateCredentials()
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "Q":
+            waiting_for_input = False
+        else:
+            print("Enter a valid choice.\n")            
+    
